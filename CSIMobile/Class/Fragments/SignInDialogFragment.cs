eb8f2f66f48bbafb664d4167695df07b8add4e01
jekingpan <jekingpan@hotmail.com>
2018-01-04 23:37:11 +0800

    update

--- CSIMobile/Class/Fragments/SignInDialogFragment.cs
+++ CSIMobile/Class/Fragments/SignInDialogFragment.cs
@@ -9,23 +9,30 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using CSIMobile.Class.Common;
 
 namespace CSIMobile.Class.Fragments
 {
-    public class SignInDialogFragment : DialogFragment
+    public class SignInDialogFragment : CSIBaseDialogFragment
     {
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
-            base.OnCreate(savedInstanceState);
+            try
+            {
+                base.OnCreate(savedInstanceState);
 
-            var view = inflater.Inflate(Resource.Layout.CSISignIn, container, false);
+                var view = inflater.Inflate(Resource.Layout.CSISignIn, container, false);
 
-            //not allow to cancel sign in process...
-            Cancelable = false;
+                //not allow to cancel sign in process...
+                Cancelable = false;
 
-            // Set up a handler to dismiss this DialogFragment when this button is clicked.
-            view.FindViewById<Button>(Resource.Id.SignInButton).Click += (sender, args) => Dismiss();
-            return view;
+                // Set up a handler to dismiss this DialogFragment when this button is clicked.
+                view.FindViewById<Button>(Resource.Id.SignInButton).Click += (sender, args) => Dismiss();
+                return view;
+            }catch (Exception Ex)
+            {
+                WriteErrorLog(Ex);
+            }
         }
 
     }
