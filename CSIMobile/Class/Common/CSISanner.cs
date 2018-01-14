cf8438fdbae5e6a06e3be45f090c82c5a92d5a46
jekingpan <jekingpan@hotmail.com>
2018-01-14 21:02:41 +0800

    add scanner

---
+++ CSIMobile/Class/Common/CSISanner.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using ZXing;
+using ZXing.Mobile;
+using System.Threading.Tasks;
+
+namespace CSIMobile.Class.Common
+{
+    class CSISanner : CSIBaseObject
+    {
+
+        public static async Task<String> ScanAsync()
+        {
+            MobileBarcodeScanningOptions opts = new MobileBarcodeScanningOptions
+            {
+                PossibleFormats = new List<BarcodeFormat>
+                {
+                BarcodeFormat.CODE_39,
+                BarcodeFormat.CODE_93,
+                BarcodeFormat.CODE_128,
+                BarcodeFormat.EAN_13,
+                BarcodeFormat.EAN_8,
+                BarcodeFormat.QR_CODE
+                }
+            };
+            MobileBarcodeScanner scanner = new MobileBarcodeScanner();
+            ZXing.Result result = await scanner.Scan(opts);
+            return result?.Text ?? string.Empty;
+        }
+    }
+}
