c7241c10fc6a37b061abda89188d7bc07efd8663
jpan2 <jeking.pan@infor.com>
2018-01-18 18:05:01 +0800

    adding printing functionality

---
+++ CSIMobile/Class/Common/CSIBluetooth.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using ZXing;
+using ZXing.Mobile;
+using System.Threading.Tasks;
+using Android.Support.V4.Print;
+using Android.Print;
+using Android.Webkit;
+using System.IO;
+using Android.Graphics;
+using Android.Bluetooth;
+using Java.IO;
+using Java.Util;
+using System.Collections.ObjectModel;
+
+namespace CSIMobile.Class.Common
+{
+    public interface IBluetooth
+    {
+        ObservableCollection<string> PairedDevices();
+
+        void Imprimir(string pStrNomBluetooth, int intSleepTime, string pStrTextoImprimir);
+    }
+
+    public class CSIBluetooth : IBluetooth
+    {
+        private BluetoothAdapter adapter = BluetoothAdapter.DefaultAdapter;
+        private BluetoothSocket socket = null;
+        private BufferedWriter outReader = null;
+        private BluetoothDevice device = null;
+
+        public void Imprimir(string pStrNomBluetooth, int intSleepTime, string pStrTextoImprimir)
+        {
+            try
+            {
+                string bt_printer = (from d in adapter.BondedDevices
+                                     where d.Name == pStrNomBluetooth
+                                     select d).FirstOrDefault().ToString();
+
+                device = adapter.GetRemoteDevice(bt_printer);
+                UUID applicationUUID = UUID.FromString("00001101-0000-1000-8000-00805f9b34fb");
+
+                socket = device.CreateRfcommSocketToServiceRecord(applicationUUID);
+                socket.Connect();
+
+                outReader = new BufferedWriter(new OutputStreamWriter(socket.InputStream));
+                outReader.Write(pStrTextoImprimir);
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+            }
+        }
+
+        public ObservableCollection<string> PairedDevices()
+        {
+            ObservableCollection<string> devices = new ObservableCollection<string>();
+
+            foreach (var bd in adapter.BondedDevices)
+                devices.Add(bd.Name);
+
+            return devices;
+        }
+    }
+}
---
+++ CSIMobile/Class/Common/CSIPrinter.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using ZXing;
+using ZXing.Mobile;
+using System.Threading.Tasks;
+using Android.Support.V4.Print;
+using Android.Print;
+using Android.Webkit;
+using System.IO;
+using Android.Graphics;
+
+namespace CSIMobile.Class.Common
+{
+    class CSIPrinter : CSIBaseObject
+    {
+        public static void doPhotoPrint()
+        {
+            PrintHelper photoPrinter = new PrintHelper(Application.Context);
+            photoPrinter.ScaleMode = PrintHelper.ScaleModeFit;
+            Bitmap bitmap = BitmapFactory.DecodeResource(null, 0);
+            photoPrinter.PrintBitmap("droids.jpg - test print", bitmap);
+        }
+
+
+        public static void doWebViewPrint() {
+            WebView mWebView;
+            // Create a WebView object specifically for printing
+            WebView webView = new WebView(Application.Context);
+            webView.SetWebViewClient(new WebViewClient());
+
+            // Generate an HTML document on the fly:
+            String htmlDocument = "<html><body><h1>Test Content</h1><p>Testing, " +
+                    "testing, testing...</p></body></html>";
+            webView.LoadDataWithBaseURL(null, htmlDocument, "text/HTML", "UTF-8", null);
+
+            // Keep a reference to WebView object until you pass the PrintDocumentAdapter
+            // to the PrintManager
+            mWebView = webView;
+        }
+
+        public static void doPrint()
+        {
+            try
+            {
+                // Get a PrintManager instance
+                PrintManager printManager = (PrintManager)Application.Context.GetSystemService(Context.PrintService);
+
+                // Set job name, which will be displayed in the print queue
+                String jobName = Application.Context.GetString(Resource.String.app_name) + "_Log.txt";
+
+                WebView myWebView = new WebView(Application.Context);
+                PrintDocumentAdapter printDocumentAdapter = myWebView.CreatePrintDocumentAdapter(jobName);
+                printManager.Print("MyWebPage", printDocumentAdapter, null);
+                // Start a print job, passing in a PrintDocumentAdapter implementation
+                // to handle the generation of a print document
+                //printManager.Print(jobName, new MyPrintDocumentAdapter(getActivity()), null); //
+
+
+                //PrintDocumentAdapter printDocumentAdapter = myWebView.CreatePrintDocumentAdapter();
+                //printManager.Print("MyWebPage", printDocumentAdapter, null);
+
+            }
+            catch (Exception Ex)
+            {
+                WriteErrorLog(Ex);
+            }
+        }
+    }
+}
